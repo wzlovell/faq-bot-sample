7c5e9accaff1aa769a9a3081ac68ff554e472975
wangzhuo <z.wang@mbpsmartec.co.jp>
2020-05-29 11:15:34 +0900

    'Improve'

---
+++ deploy.bat
@@ -0,0 +1,2 @@
+git add . && git commit -m 'Improve' && git push
+pause
--- skill/handle-pizza-order.js
+++ skill/handle-pizza-order.js
@@ -1,60 +1,133 @@
-'use strict';
+"use strict";
 
-module.exports = class HandlePizzaOrder {
+const debug = require("debug")("bot-express:skill");
+
+module.exports = class SkillHandlePizzaOrder {
 
     // コンストラクター。このスキルで必要とする、または指定することができるパラメータを設定します。
     constructor() {
         this.required_parameter = {
             pizza: {
-                message_to_confirm: {
+                message: {
                     type: "template",
-                    altText: "ご注文のピザはお決まりでしょうか？ マルゲリータ、マリナーラからお選びください。",
+                    altText: "ご注文のピザはお決まりでしょうか？ マルグリット、マリナーラからお選びください。",
                     template: {
                         type: "buttons",
                         text: "ご注文のピザはお決まりでしょうか？",
                         actions: [
-                            {type:"postback",label:"マルゲリータ",data:"マルゲリータ"},
-                            {type:"postback",label:"マリナーラ",data:"マリナーラ"}
+                            {type:"message",label:"マルグリット",text:"マルグリット"},
+                            {type:"message",label:"マリナーラ",text:"マリナーラ"}
                         ]
+                    }
+                },
+                parser: "dialogflow",
+                reaction: async (error, value, bot, event, context) => {
+                    if (error){
+                        if (value == "") return;
+
+                        bot.change_message("pizza", {
+                            type: "text",
+                            text: "恐れ入りますが当店ではマルグリータかマリナーラしかございません。どちらになさいますか？"
+                        });
+                    } else {
+                        bot.queue({
+                            type: "text",
+                            text: `${value}ですね。ありがとうございます。`
+                        });
                     }
                 }
             },
             size: {
-                message_to_confirm: {
+                message: {
                     type: "template",
                     altText: "サイズはいかがいたしましょうか？ S、M、Lからお選びください。",
                     template: {
                         type: "buttons",
                         text: "サイズはいかがいたしましょうか？",
                         actions: [
-                            {type:"postback",label:"S",data:"S"},
-                            {type:"postback",label:"M",data:"M"},
-                            {type:"postback",label:"L",data:"L"}
+                            {type:"message",label:"S",text:"S"},
+                            {type:"message",label:"M",text:"M"},
+                            {type:"message",label:"L",text:"L"}
                         ]
                     }
+                },
+                parser: async (value, bot, event, context) => {
+                    if (typeof value == "string"){
+                        return value;
+                    } else if (typeof value == "object"){
+                        return value.data;
+                    }
+
+                    throw new Error();
                 }
             },
             address: {
-                message_to_confirm: {
+                message: {
                     type: "text",
                     text: "お届け先の住所を教えていただけますか？"
+                },
+                parser: async (value, bot, event, context) => {
+                    if (typeof value == "string"){
+                        return {
+                            address: value,
+                            latitude: null,
+                            longitude: null
+                        }
+                    } else if (typeof value == "object"){
+                        if (value.address){
+                            // This is LINE location message.
+                            return {
+                                address: value.address,
+                                latitude: value.latitude,
+                                longitude: value.longitude
+                            }
+                        } else if (value.attachments){
+                            for (let attachment of value.attachments){
+                                if (attachment.type == "location"){
+                                    return {
+                                        address: null, // Need to fill out some day...
+                                        latitude: attachment.payload.coordinates.lat,
+                                        longitude: attachment.payload.coordinates.long
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    throw new Error();
                 }
             },
             name: {
-                message_to_confirm: {
+                message: {
                     type: "text",
                     text: "最後に、お客様のお名前を教えていただけますか？"
+                },
+                parser: {
+                    type: "string",
+                    policy: {
+                        max: 20
+                    }
+                },
+                reaction: async (error, value, bot, event, context) => {
+                    if (error){
+                        bot.change_message("name", {
+                            type: "text",
+                            text: "すみません、お名前は20文字まででお願いします。"
+                        })
+                    }
                 }
             }
-        };
+        }
+
+        this.take_over_parameter = true
     }
 
     // パラメーターが全部揃ったら実行する処理を記述します。
     async finish(bot, event, context){
-        let message = {
-            text: `${context.confirmed.name} 様、ご注文ありがとうございました！${context.confirmed.pizza}の${context.confirmed.size}サイズを30分以内にご指定の${context.confirmed.address}までお届けに上がります。`
-        };
-
-        await bot.reply(message);
+        const messages = [{
+            type:"text",
+            text: `${context.confirmed.name} 様、ご注文ありがとうございました！${context.confirmed.pizza}の${context.confirmed.size}サイズを30分以内にご指定の${context.confirmed.address.address}までお届けに上がります。`
+        }];
+        await bot.reply(messages);
     }
 };
